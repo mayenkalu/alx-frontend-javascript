dff813250fb033c40e9c89e2ee9078754a7bbfb1
Mayen Kalu <kalumayen@gmail.com>
2023-07-04 11:51:53 +0100

    action: updated task 3

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,11 +1,7 @@
-/* eslint-disable linebreak-style */
-import getListStudentIds from './1-get_list_student_ids';
-
-const getStudentIdsSum = (listOfStudents) => {
-  const reducer = (accumulator, currentValue) => accumulator + currentValue;
-  const studentsIds = getListStudentIds(listOfStudents);
-
-  return studentsIds.reduce(reducer);
-};
-
-export default getStudentIdsSum;
+export default function getStudentIdsSum(A) {
+    const arr = A.map((d) => {return d.id;});
+    const result = arr.reduce((a, b) => {
+        return a + b;
+    })
+    return result;
+}
